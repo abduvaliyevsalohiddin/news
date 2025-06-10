d20c242c18d9043ec063d92e2a3a739a19f1b91d
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2025-06-10 18:42:48 +0500

    cors

--- core/settings.py
+++ core/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
     # app
     'main',
+    'corsheaders',
 
     # packages
     'rest_framework',
@@ -35,12 +36,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'core.urls'
 
